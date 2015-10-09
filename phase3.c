d3f5ee8bc4c39a7ba6628f2890166752aebbc7a7
Calmage <atakagi@email.arizona.edu>
2015-10-09 11:59:35 -0700

    nathan stuff

--- phase3.c
+++ phase3.c
@@ -8,16 +8,47 @@
     -Andre Takagi
     -Alex Ewing
   ------------------------------------------------------------------------ */
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
 #include <usloss.h>
 #include <phase1.h>
 #include <phase2.h>
 #include <phase3.h>
 #include <usyscall.h>
-
 /* ------------------------- Prototypes ----------------------------------- */
 static void nullsys3(systemArgs *args);
+static void Spawn(systemArgs *args);
+static void Wait(systemArgs *args);
+static void Terminate(systemArgs *args);
+static void SemCreate(systemArgs *args);
+static void SemP(systemArgs *args);
+static void SemV(systemArgs *args);
+static void SemFree(systemArgs *args);
+static void GetTimeofDay(systemArgs *args);
+static void CPUTime(systemArgs *args);
+static void GetPID(systemArgs *args);
+
+int SpawnReal(char *name, int(*func)(char *), char *arg, int stacksize, int priority);
+int SpawnLaunch(char *args);
+int WaitReal(int *status);
+void TerminateReal(int status);
+int SemCreateReal(systemArgs *args);
+int SemPReal(systemArgs *args);
+int SemVReal(systemArgs *args);
+int SemFreeReal(systemArgs *args);
+void GetTimeofDayReal(systemArgs *args);
+void CPUTimeReal(systemArgs *args);
+void GetPIDReal(systemArgs *args);
+
 static void syscallHandler(int dec, void *args);
 void check_kernel_mode(char* name);
+/* ------------------------- Globals ------------------------------------ */
+void (*sys_vec[MAXSYSCALLS])(systemArgs *args);
+
+//process table
+//semtable
 
 /* ------------------------- Functions ------------------------------------ */
 int start2(char *arg) {
@@ -30,6 +61,23 @@ int start2(char *arg) {
     // Data structure initialization as needed...
     USLOSS_IntVec[USLOSS_SYSCALL_INT] = syscallHandler; 
 
+    // initialize sys_vec
+    // initialize everything to nullsys3
+    for (int i = 0; i < MAXSYSCALLS; i++) {
+        sys_vec[i] = nullsys3;
+    }
+
+    // fill specified ones with their specific functions
+    sys_vec[SYS_SPAWN] = Spawn;
+    sys_vec[SYS_WAIT] = Wait;
+    sys_vec[SYS_TERMINATE] = Terminate;
+    sys_vec[SYS_SEMCREATE] = SemCreate;
+    sys_vec[SYS_SEMP] = SemP;
+    sys_vec[SYS_SEMV] = SemV;
+    sys_vec[SYS_SEMFREE] = SemFree;
+    sys_vec[SYS_GETTIMEOFDAY] = GetTimeofDay;
+    sys_vec[SYS_CPUTIME] = CPUTime;
+    sys_vec[SYS_GETPID] = GetPID;
 
     /*
      * Create first user-level process and wait for it to finish.
@@ -59,7 +107,7 @@ int start2(char *arg) {
      * values back into the sysargs pointer, switch to user-mode, and 
      * return to the user code that called Spawn.
      */
-    pid = spawnReal("start3", start3, NULL,  32 * 1024, 3);
+    pid = SpawnReal("start3", start3, NULL,  4 * USLOSS_MIN_STACK, 3);
 
     /* Call the waitReal version of your wait code here.
      * You call waitReal (rather than Wait) because start2 is running
@@ -71,7 +119,92 @@ int start2(char *arg) {
 
 static void nullsys3(systemArgs *args) {
     USLOSS_Console("nullsys(): Invalid syscall %d. Halting...\n", args->number);
-    USLOSS_Halt(1);
+    USLOSS_Halt(1); // TODO change this from halt to terminate
+}
+
+static void Spawn(systemArgs *args) {
+    SpawnReal(args.arg5, args.arg1, args.arg2, args.arg3, args.arg4);    
+}
+
+static void Wait(systemArgs *args) {
+    WaitReal();
+}
+
+static void Terminate(systemArgs *args) {
+    TerminateReal();
+}
+
+static void SemCreate(systemArgs *args) {
+    SemCreateReal();
+}
+
+static void SemP(systemArgs *args) {
+    SemPReal();
+}
+
+static void SemV(systemArgs *args) {
+    SemVReal();
+}
+
+static void SemFree(systemArgs *args) {
+    SemFreeReal();
+}
+
+static void GetTimeofDay(systemArgs *args) {
+    GetTimeofDayReal();
+}
+
+static void CPUTime(systemArgs *args) {
+    CPUTimeReal();
+}
+
+static void GetPID(systemArgs *args) {
+    GetPIDReal();
+}
+
+int SpawnReal(char *name, int(*func)(char *), char *arg, int stacksize, int priority) {
+    int kidpid = fork1(name, spawnlaunch, arg, stacksize, priority);
+    // put kidpid in process table?
+}
+
+int SpawnLaunch(char *args) {
+    // create
+}
+
+int WaitReal(int *status) {
+
+}
+
+int TerminateReal(int status) {
+
+}
+
+int SemCreateReal(systemArgs *args) {
+
+}
+
+int SemPReal(systemArgs *args) {
+
+}
+
+int SemVReal(systemArgs *args) {
+
+}
+
+int SemFreeReal(systemArgs *args) {
+
+}
+
+int GetTimeofDayReal(systemArgs *args) {
+
+}
+
+int CPUTimeReal(systemArgs *args) {
+
+}
+
+int GetPIDReal(systemArgs *args) {
+
 }
 
 static void syscallHandler(int dev, void *args) {
--- sems.h
+++ sems.h
@@ -1,2 +1,12 @@
-
 #define MAX_SEMS 200
+
+typedef struct procStruct procStruct;
+
+typedef struct procStruct * procPtr;
+
+struct procStruct {
+    int status;
+    int pid;
+    int ppid;
+    int numKids;
+}
